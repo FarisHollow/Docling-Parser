ffa693014991bfae626c839535f683be164e4b83
Mir Faris <mirfaris79@gmail.com>
2024-12-17 02:39:07 +0600

    Not arranged but project done

--- chat/chat.py
+++ chat/chat.py
@@ -1,14 +1,17 @@
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 from fastapi.responses import FileResponse
-from qdrant.vector_store import index
-from utils.chat_history import chat_memory
+from utils.dataset_utils import index
+from utils.chat_history import chat_memory, chat_store, chat_store_path
+from llama_index.core.chat_engine.types import ChatMode
 from llama_index.core.tools import QueryEngineTool
 from llama_index.core.agent import ReActAgent
 from llama_index.llms.openai import OpenAI
+from utils.dataset_utils import client, my_collection
 import os
-
+import openai
 
 router = APIRouter()
+
 
 @router.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
@@ -32,13 +35,35 @@ async def websocket_endpoint(websocket: WebSocket):
         while True:
             data = await websocket.receive_text() 
             print(f"User: {data}")
+            
             await websocket.send_text(f"User: {data}")
             
-            from utils.dataset_utils import search_context 
-            enriched_data = await search_context(data)
+            from llama_index.embeddings.fastembed import FastEmbedEmbedding
             
+            embed_model = FastEmbedEmbedding(model_name="BAAI/bge-small-en-v1.5")   
+            embeddings = embed_model.get_text_embedding(data)
 
-            response = str(agent.chat(enriched_data))
+            search_result = client.search(
+               collection_name=my_collection,
+               query_vector=embeddings,
+               limit=3
+              )
+            
+    
+            context = "\n".join(
+                 text if isinstance(text, str) else " ".join(text)
+                 for hit in search_result
+                 for text in ([hit.payload['text']] if isinstance(hit.payload['text'], str) else hit.payload['text'])
+             )
+
+            print(f"Retrieved Context: {context}")
+            
+            enriched_query = f"Context:\n{context}\n\nUser Query:\n{data}"
+            response = str(agent.chat(enriched_query))
+            print(f"Reply: {response}")
+            
+            response = str(agent.chat(data))
+            
             print(f"Reply: {response}")
             await websocket.send_text(f"Reply: {response}")
             
--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-from fastapi import FastAPI
+from io import BytesIO
+from docling.datamodel.base_models import InputFormat
+from docling.datamodel.pipeline_options import PdfPipelineOptions
+from docling.document_converter import DocumentConverter, PdfFormatOption, DocumentStream
+from docling.pipeline.standard_pdf_pipeline import StandardPdfPipeline
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi.responses import HTMLResponse
+from typing import Annotated
+from pathlib import Path
+from llama_index.embeddings.fastembed import FastEmbedEmbedding
+from qdrant_client import models
+import uuid
+import numpy as np
 import contextlib 
 from dotenv import load_dotenv
 from fastapi.staticfiles import StaticFiles
-from service import chat, doc
-
+from chat import chat
+from utils.dataset_utils import client, my_collection
+from llama_index.core.node_parser import TokenTextSplitter
+from llama_index.core import Document
+import qdrant_client
 
 @contextlib.asynccontextmanager
 async def lifespan(app):
@@ -12,7 +27,121 @@ async def lifespan(app):
 app = FastAPI(lifespan=lifespan, debug=True)
 
 
+count = client.count(collection_name=my_collection).count
+print(f"Total points in collection '{my_collection}': {count}")
+
+# Get the number of vectors in the collection
+response = client.count(collection_name=my_collection)
+print(f"Number of vectors in the collection: {response.count}")
+
+
+async def docling_parse(files):
+    
+    artifacts_path = StandardPdfPipeline.download_models_hf()
+    pipeline_options = PdfPipelineOptions(artifacts_path=artifacts_path)
+    converter = DocumentConverter(
+        format_options={InputFormat.PDF: PdfFormatOption(pipeline_options=pipeline_options)}
+    )
+
+    result = converter.convert(DocumentStream(name="file", stream=BytesIO(files)))  
+    output = result.document.export_to_markdown()
+    dict_output = result.document.export_to_dict()
+    clean_output = [child['orig'] for child in dict_output['texts'] if 'orig' in child]
+    
+    # keys = [f"key{i}" for i in range(1, len(clean_output) + 1)]
+
+
+    # output_dict = dict(zip(keys, clean_output))
+          
+    # print(f"This is clean type {type(output_dict)}", output_dict)            
+
+
+    return (output, clean_output)
+
+
+async def chunking(output):
+    
+    text_splitter = TokenTextSplitter(
+        chunk_size=1000,  # Number of tokens per chunk
+        chunk_overlap=200,  
+    )
+    chunks = text_splitter.split_text(output)
+    
+    print(type(chunks), "This is type")
+            
+    return chunks
+
+
+
+        
+async def embedding(nodes):
+    
+    embed_model = FastEmbedEmbedding(model_name="BAAI/bge-small-en-v1.5")   
+    embeddings = embed_model.get_text_embedding_batch(nodes)
+    embeddings = [embeddings] if isinstance(embeddings[0], float) else embeddings
+    print("Embedding shape:", np.array(embeddings).shape)
+    
+  
+    return embeddings
+    
+    
+async def insert_db(embeddings, payloads):
+    embedding_ids = [str(uuid.uuid4()) for _ in embeddings]
+
+    try:
+     
+        client.upsert(
+            collection_name=my_collection,
+            points=models.Batch(
+                ids=embedding_ids,
+                vectors=embeddings,
+                payloads=[{'text': payloads}]
+            ),
+        )
+    except Exception as e:
+        print(f"Error occurred during insertion: {str(e)}") 
+        raise HTTPException(status_code=500, detail=f"Error occurred: {str(e)}")
+
+    print("Inserted data successfully")
+
+    
+        
+    
+
+@app.post("/upload/")
+async def create(files: Annotated[list[UploadFile], File(description="Multiple files as UploadFile")]):
+    
+
+    if not files:
+        raise HTTPException(status_code=400, detail="No files uploaded")
+
+    parse, dict_output  = await docling_parse(files=files[0].file.read())
+    
+    payloads=dict_output
+
+    print(type(parse), " This is type")
+    chunk = await chunking(output=parse)
+    
+    embed = await embedding(nodes=chunk)
+    
+
+    await insert_db(embeddings=embed, payloads=payloads)
+    
+    return {"message": parse} 
+
+
+
+@app.get("/doc", response_class=HTMLResponse)
+async def frontend():
+    file_path = Path(__file__).parent / "view" / "doc.html"
+
+    with open(file_path, "r") as f:
+        html_content = f.read()
+    
+    return HTMLResponse(content=html_content)
+
 
 app.mount("/view", StaticFiles(directory="view"), name="static")
-app.include_router(doc.router)
+
+
 app.include_router(chat.router)
